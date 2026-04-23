8393b91acf69574c79bbe18bf6738cea1df8a831
Stefan B <steftechhq@gmail.com>
2026-04-23 16:31:22 +0200

    feat: add dependency injection structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("../../ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}/{$}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}/{$}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	logger.Info("starting server", "addr", cfg.addr)
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
-
+	err := http.ListenAndServe(cfg.addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
