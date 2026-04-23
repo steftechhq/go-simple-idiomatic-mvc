53afe6d22d0ead6f097ebcdb7d4aa864a2807244
Stefan B <steftechhq@gmail.com>
2026-04-23 20:18:36 +0200

    feat: add users authorization feature

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -9,11 +9,12 @@ import (
 )
 
 type templateData struct {
-	CurrentYear int
-	Snippet     models.Snippet
-	Snippets    []models.Snippet
-	Form        any
-	Flash       string
+	CurrentYear     int
+	Snippet         models.Snippet
+	Snippets        []models.Snippet
+	Form            any
+	Flash           string
+	IsAuthenticated bool // Add an IsAuthenticated field to the templateData struct.
 }
 
 func humanDate(t time.Time) string {
